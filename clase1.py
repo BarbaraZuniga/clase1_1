d0c818ed5bbef6d44401165169537e136a385681
Barbara <barbara.zuar@gmail.com>
2024-06-01 13:11:59 -0400

    Agregar listas unidimensionales (listas)

---
+++ clase1.py
@@ -0,0 +1,8 @@
+#Arreglo unidimensionales (lista)
+#definir de una lista
+numeros =[1,2,3,4,5]
+#imprimir la lista
+print("Lista de números", numeros)
+#acceso a elementos individuales de la lista
+print("Lista de números", numeros[0]) #los indices comienzan con 0
+print("Lista de números", numeros[-1]) #accede al ultimo elemento
